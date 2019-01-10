13caf28ac542a73a0b280ffa2bd79f989a0c721d
majorinche <594216467@qq.com>
2019-01-10 16:03:48 +0800

    re-add

---
+++ OpenshiftOneClick/files/pull_cicd_images.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+CICD_IMAGES=(gitlab/gitlab-ce:11.4.0-ce.0 \
+	redis:3.2.3-alpine \
+	centos/postgresql-95-centos7:latest \
+	openshiftdemos/gogs:0.11.34 \
+	openshift/jenkins-2-centos7:latest \
+	sonatype/nexus3:3.14.0 \
+	openshift/jenkins-agent-maven-35-centos7:v4.0 \
+	openshiftdemos/sonarqube:6.7 \
+	centos/mysql-57-centos7:latest )
+
+for image in "${CICD_IMAGES[@]}"
+do
+	docker pull $image
+done
