a4ae5094e92d4240fa70adbe14fff29340ea87f3
majorinche <594216467@qq.com>
2019-10-10 08:07:45 +0800

    update

--- cluster_install/ssl_config.sh
+++ cluster_install/ssl_config.sh
@@ -1,10 +1,21 @@
 #!/bin/sh
 
-for i in `seq 2 3`;do 
-	scp -rp okd01:/etc/docker/certs.d/docker-repo.sonic.com okd00${i}:/etc/docker/certs.d/
-	ssh okd00${i} mv /etc/docker/certs.d/docker-repo.sonic.com/docker-repo.sonic.com.cert /etc/docker/certs.d/docker-repo.sonic.com/docker-repo.sonic.com.crt
-	ssh okd00${i} service docker restart
-done
+batch_copy(){
+
+	for i in `seq 2 3`;do 
+		scp -rp okd02:/etc/docker/certs.d/docker-repo.sonic.com okd00${i}:/etc/docker/certs.d/
+		ssh okd00${i} mv /etc/docker/certs.d/docker-repo.sonic.com/docker-repo.sonic.com.cert /etc/docker/certs.d/docker-repo.sonic.com/docker-repo.sonic.com.crt
+		ssh okd00${i} service docker restart
+	done
+}
+
+single_copy(){
+	scp -rp okd01:/etc/docker/certs.d/docker-repo.sonic.com /etc/docker/certs.d/
+        mv /etc/docker/certs.d/docker-repo.sonic.com/docker-repo.sonic.com.cert /etc/docker/certs.d/docker-repo.sonic.com/docker-repo.sonic.com.crt
+	service docker restart
+	cat /etc/docker/certs.d/docker-repo.sonic.com/docker-repo.sonic.com.crt >> /etc/pki/tls/certs/ca-bundle.crt
+}
 
 # ssh connection does not support redirection function, like >>
-cat /etc/docker/certs.d/docker-repo.sonic.com/docker-repo.sonic.com.crt >> /etc/pki/tls/certs/ca-bundle.crt
+#cat /etc/docker/certs.d/docker-repo.sonic.com/docker-repo.sonic.com.crt >> /etc/pki/tls/certs/ca-bundle.crt
+single_copy
