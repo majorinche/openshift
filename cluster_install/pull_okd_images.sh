662ce5a713954eeb98e5728c2d82bfb4725611b9
majorinche <594216467@qq.com>
2019-10-11 17:47:59 +0800

    add image

--- cluster_install/pull_okd_images.sh
+++ cluster_install/pull_okd_images.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-yum -y install docker && service docker restart
 docker pull docker-repo.sonic.com:443/cockpit/kubernetes:latest
 docker pull docker-repo.sonic.com:443/openshift/origin-control-plane:v3.11.0
 docker pull docker-repo.sonic.com:443/openshift/origin-deployer:v3.11.0
@@ -9,3 +8,8 @@ docker pull docker-repo.sonic.com:443/openshift/origin-haproxy-router:v3.11.0
 docker pull docker-repo.sonic.com:443/openshift/origin-pod:v3.11.0
 docker pull docker-repo.sonic.com:443/coreos/etcd:v3.2.22
 docker tag docker-repo.sonic.com:443/coreos/etcd:v3.2.22 quay.io/coreos/etcd:v3.2.22
+docker pull docker-repo.sonic.com:443/openshift/oauth-proxy:v1.1.0
+docker pull docker-repo.sonic.com:443/grafana/grafana:5.2.1
+docker tag docker-repo.sonic.com:443/openshift/oauth-proxy:v1.1.0 openshift/oauth-proxy:v1.1.0
+docker tag docker-repo.sonic.com:443/grafana/grafana:5.2.1 grafana/grafana:5.2.1
+docker pull docker-repo.sonic.com:443/openshift/origin-logging-curator5:v3.11.0
